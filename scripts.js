02eb6b4a29d8a1268ddf6cca4e998d38cb2a5f87
githubofjk <hkim317@gmail.com>
2020-12-24 14:36:55 -0800

    set container grid size based on gridWidth and gridHeight

--- scripts.js
+++ scripts.js
@@ -1,24 +1,40 @@
 const container = document.querySelector('#container');
 
+// set grid size and box sizes
+let gridWidth = 16;
+let gridHeight = 16;
+
 let boxWidthHeight = 20;
 let boxBorderWidth = 1;
 let boxSize = boxWidthHeight + boxBorderWidth;
 
-let gridWidth = 3;
-let gridHeight = 3;
+// external stylesheet's list of rules
+const styleSheetList = document.styleSheets;
+let styleSheet = styleSheetList[0];
 
+// set container grid size based on gridWidth and gridHeight
 let cssGridTemplateColumns = `grid-template-columns:`;
-let cssGridTemplateRows = "grid-template-rows: 21px 21px 21px";
-container.setAttribute('style', `${ cssGridTemplateRows }`);
-
+let cssGridTemplateRows = `grid-template-rows:`;
 
 for (i = 0; i < gridWidth; i++) {
-    cssGridTemplateColumns += ` ${boxSize}px`
-    container.setAttribute('style', `${ cssGridTemplateColumns };`);
+    cssGridTemplateColumns += ` ${boxSize}px`;
+}
+styleSheet.insertRule(`.grid-width { ${cssGridTemplateColumns}; }`, 1);
+container.classList.add('grid-width');
+
+for (i = 0; i < gridHeight; i++) {
+    cssGridTemplateRows += ` ${boxSize}px`;
+}
+styleSheet.insertRule(`.grid-height { ${cssGridTemplateRows};}`, 2);
+container.classList.add('grid-height');
+
+// create and append boxes to container
+for (i = 0; i < gridWidth; i++) {
     for (j = 0; j < gridHeight; j++) {
         const box = document.createElement('div');
+        // sets dynamic inline style instead of stylesheet class
         box.setAttribute('style', `width: ${boxWidthHeight}; height: ${boxWidthHeight}; border-width: ${boxBorderWidth};`);
-        box.classList.add('box-base');
+        box.classList.add('box');
         container.appendChild(box);
     }
 }
@@ -27,5 +43,5 @@ function trail(e) {
     e.target.classList.add('trail');
 }
 
-const boxes = Array.from(document.querySelectorAll('.box-base'));
+const boxes = Array.from(document.querySelectorAll('.box'));
 boxes.forEach(box => box.addEventListener('mouseover', trail));
