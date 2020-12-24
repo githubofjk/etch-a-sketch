4af8eb891e85e571b0f27e02386d609377627740
githubofjk <hkim317@gmail.com>
2020-12-24 13:05:13 -0800

    parameterize box size and column width

--- scripts.js
+++ scripts.js
@@ -1,16 +1,23 @@
 const container = document.querySelector('#container');
 
-let gridWidth = 1;
+let boxWidthHeight = 20;
+let boxBorderWidth = 1;
+let boxSize = boxWidthHeight + boxBorderWidth;
+
+let gridWidth = 3;
 let gridHeight = 3;
 
-let gridTemplateColumns = "grid-template-columns: 22px";
-let gridTemplateRows = "grid-template-rows: 22px 22px 22px";
-container.setAttribute('style', `${ gridTemplateColumns }`, `${ gridTemplateRows }`)
+let cssGridTemplateColumns = `grid-template-columns:`;
+let cssGridTemplateRows = "grid-template-rows: 21px 21px 21px";
+container.setAttribute('style', `${ cssGridTemplateRows }`);
 
 
 for (i = 0; i < gridWidth; i++) {
+    cssGridTemplateColumns += ` ${boxSize}px`
+    container.setAttribute('style', `${ cssGridTemplateColumns };`);
     for (j = 0; j < gridHeight; j++) {
         const box = document.createElement('div');
+        box.setAttribute('style', `width: ${boxWidthHeight}; height: ${boxWidthHeight}; border-width: ${boxBorderWidth};`);
         box.classList.add('box-base');
         container.appendChild(box);
     }
