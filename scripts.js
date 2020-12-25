4809706724a27e8d9caabaa614789c572b45b438
githubofjk <hkim317@gmail.com>
2020-12-25 09:52:20 -0800

    container grid and child boxes based on repeating 1fr

--- scripts.js
+++ scripts.js
@@ -1,45 +1,22 @@
 const btnReset = document.querySelector('#btnReset');
 const container = document.querySelector('#container');
 
-// set grid and box sizes
+// set grid, box, and border sizes
 let gridSize = 16;
 
-let boxWidthHeight = 29;
-let boxBorderWidth = 1;
-let boxSize = boxWidthHeight + boxBorderWidth;
+container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+//container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
-
+let boxBorderWidth = 0;
 let cssUnit = `px`;
-let cssBoxWidthHeight = boxWidthHeight + cssUnit;
 let cssBoxBorderWidth = boxBorderWidth + cssUnit;
 
-// external stylesheet's list of rules
-const styleSheetList = document.styleSheets;
-let styleSheet = styleSheetList[0];
-
-// create classes to set container grid size
-let cssGridTemplateColumns = `grid-template-columns:`;
-let cssGridTemplateRows = `grid-template-rows:`;
-
-for (i = 0; i < gridSize; i++) {
-    cssGridTemplateColumns += ` ${boxSize}` + cssUnit;
-}
-styleSheet.insertRule(`.grid-width { ${cssGridTemplateColumns}; }`, 1);
-container.classList.add('grid-width');
-
-for (i = 0; i < gridSize; i++) {
-    cssGridTemplateRows += ` ${boxSize}` + cssUnit;
-}
-styleSheet.insertRule(`.grid-height { ${cssGridTemplateRows};}`, 2);
-container.classList.add('grid-height');
-
 // create and append boxes to container
-
 for (i = 0; i < gridSize; i++) {
     for (j = 0; j < gridSize; j++) {
         const box = document.createElement('div');
-        // set box size with inline style
-        box.setAttribute('style', `width: ${cssBoxWidthHeight}; height: ${cssBoxWidthHeight}; border-width: ${cssBoxBorderWidth};`);
+        // show box border
+        box.setAttribute('style', `border-width: ${cssBoxBorderWidth};`);
         box.classList.add('box');
         container.appendChild(box);
     }
