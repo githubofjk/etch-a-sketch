3ad6f0ddb9932c70eb9f65b1ef82c728f181059f
githubofjk <hkim317@gmail.com>
2020-12-25 10:03:51 -0800

    create boxes in single loop instead of nested loops

--- scripts.js
+++ scripts.js
@@ -1,25 +1,23 @@
 const btnReset = document.querySelector('#btnReset');
 const container = document.querySelector('#container');
 
-// set grid, box, and border sizes
-let gridSize = 16;
-
+// set number of container columns and rows, which sets number of boxes
+let gridSize = 64;
 container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 //container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
+// change boxBorderWidth > 0 to see box borders
 let boxBorderWidth = 0;
 let cssUnit = `px`;
 let cssBoxBorderWidth = boxBorderWidth + cssUnit;
 
 // create and append boxes to container
-for (i = 0; i < gridSize; i++) {
-    for (j = 0; j < gridSize; j++) {
-        const box = document.createElement('div');
-        // show box border
-        box.setAttribute('style', `border-width: ${cssBoxBorderWidth};`);
-        box.classList.add('box');
-        container.appendChild(box);
-    }
+for (i = 0; i < (gridSize * gridSize); i++) {
+    const box = document.createElement('div');
+    // show box border
+    box.setAttribute('style', `border-width: ${cssBoxBorderWidth};`);
+    box.classList.add('box');
+    container.appendChild(box);
 }
 
 function trail(e) {
