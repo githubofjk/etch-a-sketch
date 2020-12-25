9482f9ac8b3a7410f25c6b2ca9a273bbb8e5cec6
githubofjk <hkim317@gmail.com>
2020-12-25 08:08:58 -0800

    Grid size based on just gridSize instead of gridWidth and gridHeight

--- scripts.js
+++ scripts.js
@@ -1,11 +1,10 @@
 const btnReset = document.querySelector('#btnReset');
 const container = document.querySelector('#container');
 
-// set grid size and box sizes
-let gridWidth = 16;
-let gridHeight = 16;
+// set grid and box sizes
+let gridSize = 16;
 
-let boxWidthHeight = 20;
+let boxWidthHeight = 29;
 let boxBorderWidth = 1;
 let boxSize = boxWidthHeight + boxBorderWidth;
 
@@ -18,25 +17,26 @@ let cssBoxBorderWidth = boxBorderWidth + cssUnit;
 const styleSheetList = document.styleSheets;
 let styleSheet = styleSheetList[0];
 
-// create classes to set container grid size based on gridWidth and gridHeight
+// create classes to set container grid size
 let cssGridTemplateColumns = `grid-template-columns:`;
 let cssGridTemplateRows = `grid-template-rows:`;
 
-for (i = 0; i < gridWidth; i++) {
+for (i = 0; i < gridSize; i++) {
     cssGridTemplateColumns += ` ${boxSize}` + cssUnit;
 }
 styleSheet.insertRule(`.grid-width { ${cssGridTemplateColumns}; }`, 1);
 container.classList.add('grid-width');
 
-for (i = 0; i < gridHeight; i++) {
+for (i = 0; i < gridSize; i++) {
     cssGridTemplateRows += ` ${boxSize}` + cssUnit;
 }
 styleSheet.insertRule(`.grid-height { ${cssGridTemplateRows};}`, 2);
 container.classList.add('grid-height');
 
 // create and append boxes to container
-for (i = 0; i < gridWidth; i++) {
-    for (j = 0; j < gridHeight; j++) {
+
+for (i = 0; i < gridSize; i++) {
+    for (j = 0; j < gridSize; j++) {
         const box = document.createElement('div');
         // set box size with inline style
         box.setAttribute('style', `width: ${cssBoxWidthHeight}; height: ${cssBoxWidthHeight}; border-width: ${cssBoxBorderWidth};`);
@@ -57,3 +57,5 @@ const boxes = Array.from(document.querySelectorAll('.box'));
 boxes.forEach(box => box.addEventListener('mouseover', trail));
 
 btnReset.addEventListener('click', reset);
+
+// window.addEventListener('load', grid);
